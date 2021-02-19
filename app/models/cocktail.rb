890717325b211abac2f8e15f14b32a05fd69f839
Fabio Freire <fabitwink@hotmail.com>
2021-02-19 14:45:55 +0000

    a lot of changes have been made

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,6 +2,7 @@ class Cocktail < ApplicationRecord
 	has_many :doses, dependent: :restrict_with_error
 	has_many :ingredients, through: :doses
 	validates :name, uniqueness: true, presence: true
+	has_one_attached :photo
 end
 
 # cocktail= Cocktail.first
